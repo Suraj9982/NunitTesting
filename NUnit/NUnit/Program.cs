fdf267f9c52eacbcb4a3461ddc0fe7769f4e2f65
suraj9982 <surajsk9982@gmail.com>
2021-12-03 18:12:23 +0530

    [suraj] add : It Converts the temprature

--- NUnit/NUnit/Program.cs
+++ NUnit/NUnit/Program.cs
@@ -9,7 +9,7 @@ namespace NUnit
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("CHOOSE THE OPTION : \n1.Day of_week\n2.Exit");
+                Console.WriteLine("CHOOSE THE OPTION : \n1.Day of_week\n2.Temprature Conversion\n3.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice) 
                 {
@@ -18,10 +18,14 @@ namespace NUnit
                         day.FindDayOfWeek();
                         break;
                     case 2:
+                        TempratureConversion temp = new TempratureConversion();
+                        temp.Conversion();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
-                        Console.WriteLine("Your choice should be between 1 to 2");
+                        Console.WriteLine("Your choice should be between 1 to 3");
                         break;
                 }
 
---
+++ NUnit/NUnit/TempratureConversion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit
+{
+    class TempratureConversion
+    {
+        public void Conversion()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE OPTION:1.Celsius To Fahrenheit\n2.Fahrenheit to Celcius\n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Celsius");
+                        double c = Convert.ToDouble(Console.ReadLine());
+                        double f = (c * 9 / 5) + 32;
+                        Console.WriteLine("Fahrenheit = {0}",f);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the fahrenheit");
+                        double fah = Convert.ToDouble(Console.ReadLine());
+                        double cel = (fah - 32) * 5 / 9;
+                        Console.WriteLine("Celsius = {0}",cel);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Your choice should be between 1 to 3");
+                        break;
+                }
+            }
+        }
+    }
+}
