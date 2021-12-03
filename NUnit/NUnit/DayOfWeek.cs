3963e945e45b711b4448d01090589ec0603b89cf
suraj9982 <surajsk9982@gmail.com>
2021-12-03 17:51:51 +0530

    [suraj] add : It takes in date and finds the day of the week

---
+++ NUnit/NUnit/DayOfWeek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit
+{
+    class DayOfWeek
+    {
+        public void FindDayOfWeek()
+        {
+            Console.WriteLine("enter the day");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the month");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Day is "+ FindDay(day,month,year));
+        }
+        public static int FindDay(int d,int m,int y)
+        {
+
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + (31 * m0) / 12) % 7;
+
+            return d0;
+        }
+    }
+}
---
+++ NUnit/NUnit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE THE OPTION : \n1.Day of_week\n2.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice) 
+                {
+                    case 1:
+                        DayOfWeek day = new DayOfWeek();
+                        day.FindDayOfWeek();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Your choice should be between 1 to 2");
+                        break;
+                }
+
+            }
+        }
+    }
+}
